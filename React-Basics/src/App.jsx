4df6a82686f1274899cc7703b177caeae9c705db
rohitx06 <rohitratheesh6@gmail.com>
2025-10-17 18:46:48 +0530

    Day01

---
+++ React-Basics/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+    {/* fragment */}
+        <div id="main">
+            <h1>This is a Main Div</h1>
+        </div>
+        
+        <div id="sub">
+            <h2>This is Secondary Div</h2>
+        </div>
+    </>
+  )
+}
+
+export default App
