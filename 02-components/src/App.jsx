2ae56b7ebd0cdb679bbc7a0a9b1c6020d5221080
rohitx06 <rohitratheesh6@gmail.com>
2025-10-18 17:12:34 +0530

    Learned About Components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './components/Card'
+import Nav from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Card/>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <div className="card">
+                <h1>Learning Components in React</h1>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
